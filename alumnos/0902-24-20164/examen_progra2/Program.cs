4110e45a728d201943933fd9ebbae2e7918b6a89
Andersson Milian <milianander13@gmail.com>
2025-08-16 15:29:07 -0600

    Buenas tardes Ingeniero, le entro mi examen resuelto, y primero dios estara bien, saludos inge. Bendiciones.

---
+++ alumnos/0902-24-20164/examen_progra2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[5];
+        Console.WriteLine("Ingrese 5 números enteros:");
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            bool valido = false;
+            while (!valido)
+            {
+                Console.Write($"Número {i + 1}: ");
+                string entrada = Console.ReadLine();
+
+                // Validar que sea un entero
+                if (int.TryParse(entrada, out int numero))
+                {
+                    numeros[i] = numero;
+                    valido = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Debe ingresar un número entero.");
+                }
+            }
+        }
+
+        // Calcular promedio usando la función
+        double prom = Promedio(numeros);
+
+        // Mostrar resultado con 2 decimales
+        Console.WriteLine($"El promedio es: {prom:F2}");
+    }
+
+    // Función que recibe un arreglo y devuelve el promedio
+    static double Promedio(int[] arreglo)
+    {
+        double suma = 0;
+        foreach (int num in arreglo)
+        {
+            suma += num;
+        }
+        return suma / arreglo.Length;
+    }
+}
