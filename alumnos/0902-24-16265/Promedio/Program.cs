62060df7720b8dbb63e473c5329f2bb69b122e71
DenisG888 <dgarciam57@miumg.edu.gt>
2025-08-17 20:25:14 -0600

    :boom: Este es mi aporte para el examen

---
+++ alumnos/0902-24-16265/Promedio/Program.cs
@@ -0,0 +1,33 @@
+﻿//Aca inicia mi codigo
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[5];
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write($"Ingrese un numero entero: #{i + 1}: ");
+            while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+            {
+                Console.Write("Error. Por favor iIngrese un número entero: ");
+            }
+        }
+
+        double promedio = Promedio(numeros);
+        Console.WriteLine($"El resultado del promedio es: #{promedio:F2}");
+    }
+
+    static double Promedio(int[] arreglo)
+    {
+        int suma = 0;
+        foreach (int num in arreglo)
+        {
+            suma += num;
+        }
+        return (double)suma / arreglo.Length;
+    }
+}
+
