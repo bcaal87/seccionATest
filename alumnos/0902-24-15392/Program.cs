04abc0faef53b35e3a5774b537fad5ae542d479f
Francisco Pop <fchenp@miumg.edu.gt>
2025-08-17 22:08:17 -0600

    cree una nueva carpeta, edite Contribuitors.md

---
+++ alumnos/0902-24-15392/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[5];
+        int contador = 0;
+
+        while (contador < 5)
+        {
+            Console.Write($"Ingrese el número entero #{contador + 1}: ");
+            string entrada = Console.ReadLine();
+            if (int.TryParse(entrada, out int numero))
+            {
+                numeros[contador] = numero;
+                contador++;
+            }
+            else
+            {
+                Console.WriteLine("Dato inválido. Por favor, ingrese un número entero.");
+            }
+        }
+
+        double promedio = Promedio(numeros);
+        Console.WriteLine($"El promedio es: {promedio:F2}");
+    }
+
+    static double Promedio(int[] arreglo)
+    {
+        int suma = 0;
+        foreach (int num in arreglo)
+        {
+            suma += num;
+        }
+        return (double)suma / arreglo.Length;
+    }
+}
